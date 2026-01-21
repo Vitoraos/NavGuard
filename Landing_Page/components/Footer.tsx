60549184d30200a1721b8b3342a9a73b43c66344
Vitora OS <emmykarl07@gmail.com>
2026-01-21 06:52:09 +0100

    Create Footer.tsx

---
+++ Landing_Page/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-6 px-12 bg-secondary text-white text-center">
+      &copy; {new Date().getFullYear()} DroneAPI. All rights reserved.
+    </footer>
+  )
+}
