c9e7fe1bd2ec06cb7b67b1d097199b682de8bc2a
Vitora OS <emmykarl07@gmail.com>
2026-01-21 15:12:04 +0100

    Update Footer.tsx

--- Landing_Page/components/Footer.tsx
+++ Landing_Page/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="py-6 px-12 bg-secondary text-white text-center">
-      &copy; {new Date().getFullYear()} DroneAPI. All rights reserved.
+    <footer className="px-12 py-8 bg-[#0B1320] text-textSecondary text-center">
+      &copy; {new Date().getFullYear()} NavGuard. All rights reserved.
     </footer>
-  )
+  );
 }
