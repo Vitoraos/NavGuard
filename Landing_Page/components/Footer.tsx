7f5c0cb4ebc8a2188eb772ca769e344b509b64fb
Vitora OS <emmykarl07@gmail.com>
2026-05-20 10:33:49 +0100

    Delete Landing_Page directory

--- Landing_Page/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="px-12 py-8 bg-[#0B1320] text-textSecondary text-center">
-      &copy; {new Date().getFullYear()} NavGuard. All rights reserved.
-    </footer>
-  );
-}
